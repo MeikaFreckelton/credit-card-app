c6e5cb5f3b15eec858aeac132e569233b84b0107
MeikaFreckelton <meikafreckelton@gmail.com>
2021-03-18 15:46:34 +1100

    fixing deployment

--- backend/app.js
+++ backend/app.js
@@ -19,6 +19,15 @@ app.use('/api/users', require('./routes/auth'))
 app.use('/api/cards', require('./routes/cards'))
 
 
+// if(process.env.NODE_ENV === 'production') {
+//     app.use(express.static('client/build'));
+
+//     app.get('*', (req, res) => {
+//         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+//     });
+// }
+
+
 
 
 app.get('/', (req, res) => res.send('Home Route'));
